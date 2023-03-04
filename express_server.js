f84fec402522b957f1aae6b913fa886e50ffe11a
Medhanie Alene <alenemedhanie@gmail.com>
2023-03-04 16:04:34 -0500

    correction: getting the right longURL

--- express_server.js
+++ express_server.js
@@ -7,6 +7,9 @@ const urlDatabase = {
   "b2xVn2": "http://www.lighthouselabs.ca",
   "9sm5xK": "http://www.google.com"
 };
+//console.log(urlDatabase);
+
+app.use(express.urlencoded({ extended: true }));
 
 app.get("/", (req, res) => {
   res.send("Hello!");
@@ -21,8 +24,18 @@ app.get("/urls", (req, res) => {
   res.render("urls_index", templateVars);
 })
 
+app.get("/urls/new", (req, res) => {
+  res.render("urls_new");
+});
+
+app.post("/urls", (req, res) => {
+  console.log(req.body); // Log the POST request body to the console
+  res.send("Ok"); // Respond with 'Ok' (we will replace this)
+});
+
 app.get("/urls/:id", (req, res) => {
-  const templateVars = { id: req.params.id, longURL: urlDatabase.id};
+  const templateVars = { id: req.params.id, longURL: urlDatabase[req.params.id] };
+  console.log(templateVars);
   res.render("urls_show", templateVars);
 })
 
