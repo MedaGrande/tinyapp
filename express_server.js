07b63138b24a1fb432a3f25dfc18c44f6cb117a0
Medhanie Alene <alenemedhanie@gmail.com>
2023-03-05 20:18:53 -0500

    edit post handler for a new submitted url...needs some work

--- express_server.js
+++ express_server.js
@@ -12,40 +12,44 @@ const urlDatabase = {
 //parse the body received from form into object
 app.use(express.urlencoded({ extended: true }));
 
+//a get method demo
 app.get("/", (req, res) => {
   res.send("Hello!");
 });
 
+//sends JSON format of urlDatabase
 app.get("/urls.json", (req, res) => {
   res.json(urlDatabase);
 });
 
+//renders shortURL & longURL to browser
 app.get("/urls", (req, res) => {
   const templateVars = { urls: urlDatabase };
   res.render("urls_index", templateVars);
 })
 
+//create new url page
 app.get("/urls/new", (req, res) => {
   res.render("urls_new");
 });
 
 app.post("/urls", (req, res) => {
-  //console.log(req.body);  // Log the POST request body to the console
   let shortURL = generateRandomString();
   let longURL = req.body;
+  console.log(shortURL);
   urlDatabase[shortURL] = longURL.longURL;
   res.redirect(`/urls/${shortURL}`);
 });
+
+//delete
 app.post("/urls/:id/delete", (req, res) => {
   delete urlDatabase[req.params.id];
   res.redirect("/urls");
 });
 
+//edit
 app.post("/urls/:id", (req, res) => {
- // delete urlDatabase[req.params.id];
-  //res.redirect("/urls/:id");
   urlDatabase[req.params.id] = req.body.longURL;
-  console.log(req.params.id);
   res.redirect("/urls");
 });
 
@@ -54,25 +58,15 @@ app.get("/u/:id", (req, res) => {
   res.redirect(longURL);
 });
 
+//homepage
 app.get("/urls/:id", (req, res) => {
   const templateVars = { id: req.params.id, longURL: urlDatabase[req.params.id] };
-  console.log("templateVars", templateVars);
   res.render("urls_show", templateVars);
 })
 
 app.get("/hello", (req, res) => {
   res.send("<html><body>Hello <b>World</b></body></html>\n");
 });
-
-
-// app.get("/set", (req, res) => {
-//   const a = 1;
-//   res.send(`a set ${a}`);
-// });
-
-// app.get("/fetch", (req, res) => {
-//   res.send(`a = ${a}`);
-// });
 
 function generateRandomString() {
   return Math.random().toString(36).slice(2, 8);
