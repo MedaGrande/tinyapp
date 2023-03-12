a458fed2c5974a99e8c4e75838854d7223532b22
Medhanie Alene <alenemedhanie@gmail.com>
2023-03-11 21:08:23 -0500

    some refactoring

--- helpers.js
+++ helpers.js
@@ -1,10 +1,10 @@
-const getUserByEmail = function(email, users){
+const getUserByEmail = function(email, users) {
   for (const user in users) {
     if (users[user].email === email) {
-      return users[user];
+      return user;
     }
   }
   return null;
-}
+};
 
 module.exports = { getUserByEmail };
