c76c2ae0ba2e204d98703f5beb12079f43eb1ac8
Medhanie Alene <alenemedhanie@gmail.com>
2023-03-12 21:51:58 -0400

    move functions urlsForUser and generateRandomString to helpers.js

--- helpers.js
+++ helpers.js
@@ -1,4 +1,6 @@
-const getUserByEmail = function(email, users) {
+const { urlDatabase } = require("./database");
+
+const getUserByEmail = function (email, users) {
   for (const user in users) {
     if (users[user].email === email) {
       return user;
@@ -7,4 +9,25 @@ const getUserByEmail = function(email, users) {
   return null;
 };
 
-module.exports = { getUserByEmail };
+//filter urls that belong to a particular id
+const urlsForUser = (id) => {
+  const urls = {};
+  for (const shortID in urlDatabase) {
+    const url = urlDatabase[shortID];
+    if (url.userID === id) {
+      urls[shortID] = url;
+    }
+  }
+  return urls;
+};
+
+//generate random id
+function generateRandomString() {
+  return Math.random().toString(36).slice(2, 8);
+};
+
+module.exports = {
+  getUserByEmail,
+  urlsForUser,
+  generateRandomString
+};
